aea3e02ba3983f709cdfaa1fd06cc75753f5aead
Trevor Mugo <trevormugolawrence@gmail.com>
2025-10-08 07:31:24 +0000

    no the bg of the whole page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-[100dvh] flex-col bg-background">
+    <div className="flex min-h-[100dvh] flex-col bg-white">
       <Header />
       <main className="flex-1">
         <Hero />
