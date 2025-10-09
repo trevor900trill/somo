dea8ba4e37e8852170cb0e7a01545269440a2b0b
Trevor Mugo <trevormugolawrence@gmail.com>
2025-10-09 08:25:44 +0000

    favicon new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'Somo Hub',
+  title: 'Tengeneza na Somo',
   description: 'Empowering Artisans, Building Futures',
 };
 
