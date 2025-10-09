178d21eaa5361be7af0ca904e76453d65e96ef48
Trevor Mugo <trevormugolawrence@gmail.com>
2025-10-09 08:38:39 +0000

    return this,                 src="https://www.google.com/maps/embed?pb=!

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -37,7 +37,7 @@ export function Location() {
           <div className="flex items-center justify-center">
             <div className="w-full h-[400px] rounded-xl overflow-hidden shadow-lg">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.948831929027!2d36.96728967597373!3d-1.1395043354964175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f3f4b182d3e3d%3A0x68b913645229dc1d!2sSomo!5e0!3m2!1sen!2ske!4v1722434567890!5m2!1sen!2ske"
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3988.948842491101!2d36.96729407496556!3d-1.1395097290625946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMcKwMDgnMjIuMiJTIDM2wrA1OCcxMS41IkU!5e0!3m2!1sen!2ske!4v1722426339194!5m2!1sen!2ske"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
